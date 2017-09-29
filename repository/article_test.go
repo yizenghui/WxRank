a3e3f400c9cc52fb40d404d0fba01f1d5f19ece4
Administrator <245561237@qq.com>
2017-09-29 17:35:10 +0800

    save  article test

--- repository/article_test.go
+++ repository/article_test.go
@@ -4,8 +4,14 @@
 
 package repository
 
-import "testing"
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func Test_Post(t *testing.T) {
 	Post("https://mp.weixin.qq.com/s/e_0BhJ0vOvIqt610MjgKLA")
@@ -22,4 +28,13 @@ func Test_Hot(t *testing.T) {
 func Test_New(t *testing.T) {
 	articles, _ := New()
 	fmt.Println(articles)
+}
+
+func Test_PostLink(t *testing.T) {
+	link := url.QueryEscape("https://mp.weixin.qq.com/s?__biz=MjM5NzgzNTUyNA==&mid=2650373560&idx=1&sn=84964bd5ce47084ae175806a4bf279da&chksm=bede3bd389a9b2c540644ba0161a5ece8f5c84b3088da9eccc3d8d0712e21cf31f6a637b4940#rd")
+	doc, err := goquery.NewDocument(fmt.Sprintf("http://localhost:8888/post?url=%v", link))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(doc.Html())
 }
